300a09da78dc2479f110f684163f47a0654ed3d4
stefanutanh <bergs83@gmail.com>
2024-12-22 13:57:45 +0100

    first update

---
+++ script.js
@@ -0,0 +1,2 @@
+// URL till API-servern
+const url = "http://localhost:3000/addstuff";
---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const server = express();
+const sqlite3 = require("sqlite3");
+
+server
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+
+    next();
+  });
+
+  server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
