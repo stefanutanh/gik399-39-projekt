754b5fb1e879c1fdcadd869137b1d096803f4cd4
KattenH <h23kathe@du.se>
2025-01-09 15:47:35 +0100

    Uppdatering 9/1

--- client/script.js
+++ client/script.js
@@ -1,5 +1,11 @@
 const url = "http://localhost:3000/films";
 
+function fetchData() {
+    const existingUl = document.querySelector('ul');
+    if (existingUl) {
+        existingUl.remove();
+    }
+
 fetch(url)
     .then((response) => {
         return response.json(); // Översätt svaret till JSON
@@ -15,6 +21,20 @@ fetch(url)
             const li = document.createElement('li'); // Skapa ett li-element
             // Skapa innehållet för varje li-element
             li.innerHTML = `Title: ${film.title} // Year: ${film.year} // Director: ${film.director} // Genre: ${film.genre}`; // Skriv ut användarens namn och användarnamn
+            
+            const deleteButton = document.createElement('button');
+            deleteButton.textContent = "Ta bort"; // Texten på knappen
+            li.appendChild(deleteButton);
+
+            const changeButton = document.createElement('button');
+            changeButton.textContent = "Ändra"; // Texten på knappen
+            li.appendChild(changeButton);
+
+            // function deleteFilms(id) {
+            //     console.log('delete', id);
+            //     fetch(`${url}/${id}`, { method: 'DELETE'}).then((result) => fetchData());
+            // }
+
             ul.appendChild(li); // Lägg till li-elementet i ul-elementet
         });
 
@@ -25,19 +45,29 @@ fetch(url)
         console.error("Error fetching data:", error);
     });
 
-const submitButton=document.querySelector('#submitButton');
-const form = document.querySelector('#filmForm');
+}
 
-// console.log(submit);
-submitButton.addEventListener('click', handleSubmit);
+fetchData();
 
-function handleSubmit(e) {
+    const submitButton=document.querySelector('#submitButton');
+    const form = document.querySelector('#filmForm');
+
+    // console.log(submit);
+    submitButton.addEventListener('click', handleSubmit);
+
+    function handleSubmit(e) {
     e.preventDefault();
     
     const title = document.getElementById('titleInput').value;
     const year = document.getElementById('yearInput').value;
     const director = document.getElementById('directorInput').value;
     const genre = document.getElementById('genreInput').value;
+
+
+    if (!title || !year || !director || !genre) {
+        alert("Alla fält måste fyllas i innan du kan lägga till en film!");
+        return;
+       } 
 
     const serverFilmsObject = {
         title: title,
@@ -61,8 +91,28 @@ function handleSubmit(e) {
             }
             console.log("Film added successfully");
             document.getElementById('filmForm').reset(); // Återställ formuläret
+            fetchData();
+            alert("Filmen har lagts till!")
+           
         })
-        .catch((error) => {
+        .catch((error) => { 
             console.error("Error adding film:", error);
         });
+
+        return false;
     }
+    
+    const deleteButton = document.createElement('button');
+    deleteButton.addEventListener('click', deleteFilms);
+    // changeButton.addEventListener('click', changeFilms);
+    
+    function deleteFilms(e) {
+        e.preventDefault();
+    }
+
+    // function changeFilms(e) {
+    //     e.preventDefault();
+
+    //     const filmId = document.getElementById('
+    // }
+
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const server = express();
 const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./projekt.db");
 
 server
   .use(express.json())
@@ -33,16 +34,34 @@ server.post("/films", (req, res) => {
   const db = new sqlite3.Database("./projekt.db");
   const sql = `INSERT INTO films (title, year, director, genre) VALUES (?, ?, ?, ?)`;
 
-    db.run(sql, Object.values(films), (err) => {
+    db.run(sql,Object.values(films), (err) => {
     if (err) {
       console.log(err);
       res.status(500).send(err);
     } else {
-      res.send("Film added successfully!");
+      res.send("Film tillagd");
     }
   });
   db.close();
 });
+
+
+server.delete("/films/:id", (req, res) => {
+  const id = req.params.id; 
+  const sql = `DELETE FROM films WHERE id = ${id}`;
+
+    db.run(sql, (err) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else {
+      res.send("Film borttagen");
+    }
+  });
+});
+
+
+server.put
 
 server.listen(3000, () =>
   console.log("Running server on http://localhost:3000")
